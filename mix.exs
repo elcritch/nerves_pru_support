992d5ac7ac211b41c4f51dc7fc76115d573b5d23
Jaremy Creechley <creechley@gmail.com>
2018-02-24 23:00:47 -0700

    updates

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule Pru.Mixfile do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:elixir_make, "~> 0.4.0", runtime: false},
-      {:toolchain_extras, "~> 0.1", github: "elcritch/toolchain_extras", runtime: false}
+      {:toolchain_extras, "~> 0.1", github: "elcritch/toolchain_extras", runtime: false},
       {:toolchain_extras_pru_cgt, "~> 2.2.1",
        github: "elcritch/extras_toolchain_pru_cgt",
        branch: "v1.0.0rc"},
